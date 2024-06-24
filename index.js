ff23984ad1c9ed3050ddd2f7fe782ac879e62f8e
Joeken10 <joekentafir@gmail.com>
2024-06-24 19:03:10 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1; 
+
+let num3 = 28;
+let num4 = 6;
+
+let mod = num3 % num4;
+
+let numberArray = [3, 8, 12, 20];
+
+let max = Math.max(...numberArray);
